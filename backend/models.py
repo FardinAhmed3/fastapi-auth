8059953aaef9ca4b780645577d17fe702e9e8f6f
FardinAhmed3 <ahmedfardin077@gmail.com>
2025-02-24 22:03:42 -0500

    Fixed models.py

--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,9 @@
 from sqlalchemy import Column, Integer, String
-from database import Base
-from database import engine
+from database import Base, engine
 
 class User(Base):
     __tablename__ = "users"
     
-    id = Column(Integer, primary_key=True, index = True)
-    username = Column(String, unique=True, index = True)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-
-
-User.metadata.create_all(bind=engine)
