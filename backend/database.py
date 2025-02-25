2d51571c83300c139bdac840d78c1f40cdcaba79
FardinAhmed3 <ahmedfardin077@gmail.com>
2025-02-24 21:57:44 -0500

    Test

---
+++ backend/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:bionic@192.168.0.4:3306/auth"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connected_artgs={"check_same_thread": Falsee}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
